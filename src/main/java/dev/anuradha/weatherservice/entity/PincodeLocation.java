0b00ad9794ae4e98888cdf738a11a7d8a18442fd
Anuradha Belgaonkar <annubelgaonkar@gmail.com>
2025-08-19 15:54:59 +0530

    Creating project structure and adding entity and few other classes

---
+++ src/main/java/dev/anuradha/weatherservice/entity/PincodeLocation.java
@@ -0,0 +1,4 @@
+package dev.anuradha.weatherservice.entity;
+
+public class PincodeLocation {
+}
---
+++ src/main/java/dev/anuradha/weatherservice/repository/PincodeLocationRepository.java
@@ -0,0 +1,4 @@
+package dev.anuradha.weatherservice.repository;
+
+public class PincodeLocationRepository {
+}
